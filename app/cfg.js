a5427c75e4c2de4dc26a5c44ea3e560e69d4913a
gaoq <gaoq@rose-hulman.edu>
2020-08-17 16:33:04 -0400

    complete eliminate eplison function in cfg.js

--- app/cfg.js
+++ app/cfg.js
@@ -100,6 +100,7 @@ cfg.checkGrammar = function(grammar){
     grammar.ifchecked = true;
     return true;
 }
+
 cfg.checkSymbol = function(nonterminals, rules, symbol){
 
 }
@@ -154,17 +155,15 @@ cfg._eliminateEmpty = function(nonterminals, rules, nonterminalAlphabet){
         if(rules[i].length > 1){
         var temp = rules[i];
         var ifDifferent = false;
-        var newRule = [];
+        var nullableIndexes = [];
         for (j=0;j<temp.length;j++){
             if(util.contains(nullables, temp[j])){
                 ifDifferent = true;
-            }else{
-                newRule.push(temp[j]);
+                nullableIndexes.push(j);
             }
         }
-        if(ifDifferent && newRule.length != 0 && !util.contains(rules, newRule) && !(newRule.length == 1 && newRule[0].localeCompare(nonterminals[i]))){
-            nonterminals.push(nonterminals[i]);
-            rules.push(newRule);
+        if(ifDifferent){
+            cfg.modifyRule(temp, rules, nonterminals, nonterminals[i], nullableIndexes);
         }
 
     }else{
@@ -179,10 +178,29 @@ cfg._eliminateEmpty = function(nonterminals, rules, nonterminalAlphabet){
 
 }
 
+cfg.modifyRule = function(rule, arr, nonterminals, symbol, indexes){
+    for (i = 0;i<indexes.length;i++){
+        var arr1 = util.clone(indexes);
+        for(j = i+1;j<arr1.length;j++){
+            arr1[j] = arr1[j] - 1;
+        }
+        nonterminals.push(symbol);
+        arr1.splice(i,1);
+        var ruleNext = util.clone(rule);
+        ruleNext.splice(arr1[i],1);
+        if(!util.contains(arr, ruleNext) && !(ruleNext.length == 1 && ruleNext[0].localeCompare(symbol))){
+            arr.push(ruleNext);
+            if(ruleNext.length>1 && arr1.length != 0){
+            cfg.modifyRule(ruleNext, arr, nonterminals, symbol, arr1);
+            }
+        }
+    }
+}
+
 var names = {};
 name1 = "a1";
 name2 = 'a2';
-console.log(typeof name2);
-console.log(typeof name1);
+//console.log(typeof name2);
+//console.log(typeof name1);
 names[name1] = 5;
-console.log(cfg.createCFG());
+//console.log(cfg.createCFG());
